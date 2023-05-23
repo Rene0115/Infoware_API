59346da60750c92b15dbef751b3e177352caea43
Rene0115 <enere0115@gmail.com>
2023-05-23 01:18:23 +0100

    setup middleware

--- src/app.js
+++ src/app.js
@@ -24,3 +24,4 @@ const start = () => {
 };
 
 start();
+export default logger
---
+++ src/middleware/middleware.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import errorHandler from "./error.middleware.js";
+import morgan from "morgan";
+
+const middleware = (app) => {
+  // Enable CORS
+  app.use(cors());
+
+  // Logging
+  app.use(morgan("dev"));
+
+  // Parse incoming JSON data
+  app.use(express.json());
+
+  // Parse incoming URL-encoded data
+  app.use(express.urlencoded({ extended: true }));
+
+  // Register the router
+  app.use();
+
+  // Handle Errors
+  app.use(errorHandler);
+
+  // Fallback route handler
+  app.use("*", (req, res) => {
+    res.status(200).send("Server is Running Check API docs");
+  });
+};
+
+export default middleware;
